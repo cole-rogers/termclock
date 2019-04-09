4ad2e0f9f4710dca4b5f9a16fb4dcb6f8c086349
retiform <colerogers@protonmail.com>
2019-04-08 22:46:24 -0400

    ithings

---
+++ main.py
@@ -0,0 +1,15 @@
+from random import randint
+from asciimatics.screen import Screen
+
+def main(screen):
+    FontHW = 2;
+    while True:
+
+        	y=sin(r*PI/180*6)*hand_max+sYcen;
+	draw_point(x,y,c);
+        ev = screen.get_key()
+        if ev in (ord('Q'), ord('q')):
+            return
+        screen.refresh()
+
+Screen.wrapper(demo)
