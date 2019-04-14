cda9123a0726873fad4256d2d4eaf2dec07ba2be
retiform <colerogers@protonmail.com>
2019-04-14 11:46:47 -0400

    stuff

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from random import randint
 from asciimatics.screen import Screen
+import math
+from time import sleep
+#def drawCircle():
+#    font_ratio = 2;
+#    while True:
 
-def drawCircle():
-    FontHW = 2;
-    while True:
-
-    ##    	y=sin(r*PI/180*6)*hand_max+sYcen;
-##	draw_point(x,y,c);
-        ev = screen.get_key()
-        if ev in (ord('Q'), ord('q')):
-            return
-        screen.refresh()
-def defineParams():
+        #yplotpoint=sin(r*math.pi/180*6)*circleWide+YMax;
+        #print_at(,x,y);
+ #       ev = screen.get_key()
+  #      if ev in (ord('Q'), ord('q')):
+          #  return
+     #   screen.refresh()
+def defineParams(screen):
    dimTup = screen.dimensions
-   screen.print_at(dimTup,0,0) 
+   screen.print_at(str(dimTup),0,0) 
+   screen.refresh()
+   sleep(20)
 
-def main(screen):
-    drawCircle()
-    defineParams()
-Screen.wrapper(screen)
+Screen.wrapper(defineParams)
