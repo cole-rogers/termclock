c8d4e0f56462efceaf88a00d17c31f560b2ebeae
retiform <colerogers@protonmail.com>
2019-04-14 14:17:26 -0400

    CIRLCE WORKS

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+# -*- coding: utf-8 -*-
 from random import randint
 from asciimatics.screen import Screen
 import math
 from time import sleep
-def drawCircle(screen, max_circle):
+def drawCircle(screen, max_circle, x_cen, y_cen):
 #    font_ratio = 2;
-    for i in range(1,max_circle):
-        y =math.sin(i*math.pi/180*6)*max_circle+10
-        x =math.sin(i*math.pi/180*6)*max_circle+10
-        
+    for i in range(1,60):
+        y =math.sin(i*math.pi/180*6)*max_circle+x_cen
+        x =math.cos(i*math.pi/180*6)*2*max_circle+y_cen
         screen.print_at("*",round(x),round(y))
  #       ev = screen.get_key()
   #      if ev in (ord('Q'), ord('q')):
@@ -16,10 +16,18 @@ def drawCircle(screen, max_circle):
 
 def defineParams(screen):
    dimTup = screen.dimensions
-   return dimTup
+   x_cen = round(dimTup[0]/2)
+   y_cen = round(dimTup[1]/2)
+   if(x_cen*2 > y_cen):
+       max_radius = round(y_cen/2)
+   else:
+       max_radius = x_cen
+   dimData = (dimTup[0],dimTup[1],x_cen, y_cen, max_radius)
+   return dimData
 def mainScreen(screen):
-    dimTup = defineParams(screen)
+
     while True:
-        drawCircle(screen,10)
+        dimData = defineParams(screen)
+        drawCircle(screen,dimData[4], dimData[2], dimData[3])
 
 Screen.wrapper(mainScreen)
